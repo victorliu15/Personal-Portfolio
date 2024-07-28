684eae7f7e9a3073b89ea8046c917dacb571e880
Victor Liu <victorliu5000@gmail.com>
2024-07-28 11:20:42 -0400

    contact page done

--- contact.js
+++ contact.js
@@ -62,3 +62,44 @@ document.getElementById('copyGithub').addEventListener('click', function() {
         popup.classList.add('hidden');
     }, 2000);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+    
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+        if (firstName != "" && lastName != "" && email != "" && message != "") {
+            emailjs.send("service_hr75rbf", "template_kuhpxmd", {
+                from_name: firstName + " " + lastName,
+                from_email: email,
+                message: message,
+            }).then(function(response) {
+                const popup = document.getElementById('sentPopup');
+                popup.classList.remove('hidden');
+                popup.classList.add('block');
+                setTimeout(function() {
+                    popup.classList.remove('block');
+                    popup.classList.add('hidden');
+                }, 2000);
+                document.getElementById("firstName").value = "";
+                document.getElementById("lastName").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+            });
+        }
+        else {
+            const popup = document.getElementById('errorPopup');
+            popup.classList.remove('hidden');
+            popup.classList.add('block');
+            setTimeout(function() {
+                popup.classList.remove('block');
+                popup.classList.add('hidden');
+            }, 2000);
+        }
+    });
+  });
+  
