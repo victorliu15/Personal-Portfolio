fad573d01cc48eb3929d5acecf35862cc34686dd
Victor Liu <victorliu5000@gmail.com>
2024-07-29 15:15:43 -0400

    projects done

---
+++ project.js
@@ -0,0 +1,15 @@
+document.getElementById('project1').addEventListener('click', function() {
+    window.open("https://devpost.com/software/lingly", "_blank");
+});
+document.getElementById('project2').addEventListener('click', function() {
+    window.open("https://devpost.com/software/unifly", "_blank");
+});
+document.getElementById('project3').addEventListener('click', function() {
+    window.open("https://devpost.com/software/vike", "_blank");
+});
+document.getElementById('project4').addEventListener('click', function() {
+    window.open("https://github.com/victorliu15/random-background-generator", "_blank");
+});
+document.getElementById('project5').addEventListener('click', function() {
+    window.open("https://github.com/victorliu15/weather-extension", "_blank");
+});
